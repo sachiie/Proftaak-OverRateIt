5cce62d192adccc397048c8617be024cf6f0cd7d
Unknown <naomitjadiwangsa@hotmail.com>
2017-05-16 14:00:11 +0200

    Homepage
    Started on the homepage and made a fitting nav-bar.

---
+++ inc/nav-bar.php
@@ -0,0 +1,16 @@
+<ul>
+  <li><a class="logo" href="index.php"><img src="img/logo_overrateit.png"></a></li>
+  <li><a class="logo-text">OverRateIt</a></li>
+  <li><a class="active" href="index.php">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
+
+<?php
+include("style.scss");
+?>
+
+<?php
+include("default.js");
+?>
