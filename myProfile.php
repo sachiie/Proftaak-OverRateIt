5ba25e1f70cb892d264a1c37fa6b506da67037e7
Unknown <naomitjadiwangsa@hotmail.com>
2017-05-17 09:05:59 +0200

    User profile
    The starting of the user profile

---
+++ myProfile.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <link href="https://fonts.googleapis.com/css?family=Libre+Franklin" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="img/pokeballicon.ico">
+    <title>OverRateIt | It Could Be Overrateit</title>
+  </head>
+</head>
+<body>
+    <div id="main-container">
+
+    </div>
+</body>
+</html>
