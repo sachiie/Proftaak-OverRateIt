842064b44474bee6ea2db77170424502c09f7ef8
Unknown <naomitjadiwangsa@hotmail.com>
2017-05-17 09:43:28 +0200

    Nav-bar
    Added a href to all items.

--- inc/nav-bar.php
+++ inc/nav-bar.php
@@ -2,8 +2,8 @@
   <li><a class="logo" href="index.php"><img src="img/logo_overrateit.png"></a></li>
   <li><a class="logo-text">OverRateIt</a></li>
   <li><a class="active" href="index.php">Home</a></li>
-  <li><a href="#login">Login/Register</a></li>
-  <li><a href="#news">News</a></li>
+  <li><a href="#login" href="login_register.php">Login/Register</a></li>
+  <li><a href="#news" href="news.php">News</a></li>
   <li><a href="#about">About</a></li>
 </ul>
 
