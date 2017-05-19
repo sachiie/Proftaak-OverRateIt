eafd6f04fb05df0e218dedf751779e06eaeeac25
Unknown <naomitjadiwangsa@hotmail.com>
2017-05-19 13:08:34 +0200

    Nav-bar
    Added another item in the navbar

--- inc/nav-bar.php
+++ inc/nav-bar.php
@@ -3,6 +3,7 @@
   <li><a class="logo-text">OverRateIt</a></li>
   <li><a class="active" href="index.php">Home</a></li>
   <li><a href="#login" href="login_register.php">Login/Register</a></li>
+  <li><a href="#news" href="news.php">Search</a></li>
   <li><a href="#news" href="news.php">News</a></li>
   <li><a href="#about">About</a></li>
 </ul>
