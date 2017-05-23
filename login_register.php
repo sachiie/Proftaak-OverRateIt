79d01356f5bb530d34480a82cd42ddf581f2d6d8
Unknown <naomitjadiwangsa@hotmail.com>
2017-05-23 10:42:16 +0200

    References
    Added new references for Milligram and changed a few things in the CSS.

--- login_register.php
+++ login_register.php
@@ -8,6 +8,14 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+				<!-- Google Fonts -->
+		<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+
+		<!-- CSS Reset -->
+		<link rel="stylesheet" href="//cdn.rawgit.com/necolas/normalize.css/master/normalize.css">
+
+		<!-- Milligram CSS minified -->
+		<link rel="stylesheet" href="//cdn.rawgit.com/milligram/milligram/master/dist/milligram.min.css"
     <link rel="stylesheet" href="css/style.css">
     <link rel="shortcut icon" href="img/pokeballicon.ico">
     <title>OverRateIt | It Could Be Overrateit</title>
